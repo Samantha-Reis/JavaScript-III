5e6cff826e2bfe01324d268a25eaa989d2a7e7ab
Samantha-Reis <Samantha-Reis@users.noreply.github.com>
2019-09-10 20:05:31 -0400

    This questions don

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,11 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding is just an object that will always be in the global scope
+* 2. Implicit binding is when a dot is between "this" and a word in a function
+* 3. New Binding is when a constructor function is used which is when a function 
+returns an object. The new object from that is what the new binding is talking about.
+* 4. Explicit Binding is when .call .bind or .apply are added to the function
 *
 * write out a code example of each explanation above
 */
